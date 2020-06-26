6ab628b5bc2a8b55443e633d34d97d866275920b
Bhargav S <bhargavs271100@gmail.com>
2020-06-26 21:51:26 +0530

    Update DECIMAL_UP_DOWN_COUNTER.asm

--- DECIMAL_UP_DOWN_COUNTER.asm
+++ DECIMAL_UP_DOWN_COUNTER.asm
@@ -1,17 +1,31 @@
 org 00h
-mov r0,#30H
-mov r1,#90H
-loop:   mov a,r1
-		add a,#0h
-		da a
-		cjne a,#99H,BCD
-		mov r1,#0H
-		BCD:    cjne @r0,#00H,down
-				cjne @r0,#0FFH,up
-				up: 	inc r1
-						sjmp loop
-				down:   dec r1
-						sjmp loop
+MOV R0,#30H
+MOV R1,#31H
+loop:   MOV A,@R0
+		ADD A,#0H
+		DA A
+		MOV 02,@R1
+		CJNE R2,#00H,up
+		SJMP down
+		
+		up: INC A
+			ADD A,#0H
+			DA A
+			MOV R3,A
+			CJNE A,#99H,up
+			LJMP H
+		down :	DEC A
+				MOV R2,A
+				ANL A,#0FH
+				CJNE A,#0FH,here
+				SUBB A,#06H
+				here : ADD A,#00H
+				ANL 02,#0F0H
+				ORL A,R2
+				DA A
+				MOV R3,A
+				CJNE A,#00H,down
+		H :SJMP loop
 sjmp loop
 
 end
